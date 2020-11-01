da3e457a347add6c6801815f696b86a931f2b75c
hzheng-nju <hzheng-nju@smail.nju.edu.cn>
2020-11-01 15:55:41 +0800

    first commit

---
+++ src/example/InvertedIndexer.java
@@ -0,0 +1,28 @@
+package example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class InvertedIndexer {
+    public static void main(String[] args)
+    {
+        try {
+            Configuration conf = new Configuration();
+            Job job = new Job(conf, "invert index");
+            job.setJarByClass(InvertedIndexer.class);
+            job.setInputFormatClass(TextInputFormat.class);
+            job.setMapperClass(InvertedIndexMapper.class);
+            job.setReducerClass(InvertedIndexReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+}
